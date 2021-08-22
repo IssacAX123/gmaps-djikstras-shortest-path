44a454891fc2e431ccc710e848b00e0d4dca2fd0
Issac Abraham <issaca32@gmail.com>
2021-08-22 22:29:30 +0100

    centers text field

--- src/Controller.java
+++ src/Controller.java
@@ -2,15 +2,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 public class Controller {
 
     @FXML
-    public TextArea addressField;
     public Button addressSubmitBtn;
     public ImageView mapImageView;
+    public TextField addressField;
 
     int[][] points = new int[2][2];
 
